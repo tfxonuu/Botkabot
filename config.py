1c8aacc25f8163821d3d7e0f227047aacdf88d35
tfxonuu <iamtfsonu@gmail.com>
2025-11-28 19:52:15 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,5 @@
+api_id = "27293287"
+api_hash = "052041b03a7028b3bd11da3143cffa0e"
+bot_token = "8372723834:AAGmqvVPteYgaBmdWtlXomoo1oDivbcUshU"
+auth_users = [7533484450]
+sudo_users = [7533484450]
